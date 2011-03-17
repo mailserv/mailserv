d6e8fcf7e069cd39160f2caed0b5feecfed36ac5
Johan Allard <johan@allardsoft.com>
2011-03-17 22:18:26 +1100

    Fixed load error.

--- admin/config/initializers/load_config.rb
+++ admin/config/initializers/load_config.rb
@@ -8,7 +8,10 @@ include FileUtils
 #require 'net/sftp'
 require 'fastercsv'
 
-# Ensure that the database is tweaked properly 
-if %x{sudo /usr/local/bin/mysqldump mail users -d}.to_s.match(/AUTO_INCREMENT=(\d+)/)[1].to_i < 2000 rescue false
-  %x{sudo /usr/local/bin/mysql mail -e "ALTER TABLE users AUTO_INCREMENT = 2000;"}
-end if Rails.env.production?
+# Ensure that the database is tweaked properly
+begin
+  if %x{sudo /usr/local/bin/mysqldump mail users -d}.to_s.match(/AUTO_INCREMENT=(\d+)/)[1].to_i < 2000
+    %x{sudo /usr/local/bin/mysql mail -e "ALTER TABLE users AUTO_INCREMENT = 2000;"}
+  end if Rails.env.production?
+rescue
+end
