d012e25c2e60645a759fccc9411d6e1f2d65e6cd
dagn <david@gnojek.net>
2012-07-05 00:56:48 +0200

    install.sh - detect manual changes in devel branch

--- install/install.sh
+++ install/install.sh
@@ -8,6 +8,9 @@ fi
 # git checkout branch for supported OpenBSD version or development branch
 if [ "`echo $1`" == "--devel" ]; then 
   checkout_switch="devel"
+  # detect changes in devel branch 
+  branch_changes=`git --git-dir=/var/mailserv/.git --work-tree=/var/mailserv status -s`
+
 elif [ "`echo $1`" == "--help" ]; then
   echo "Usage: install.sh [OPTION]"
   echo " "
@@ -20,13 +23,29 @@ else
   checkout_switch=`uname -r`
 fi
 
-git --git-dir=/var/mailserv/.git --work-tree=/var/mailserv checkout $checkout_switch 2>/dev/null               
-if [[ `echo $?` -ne 0 ]]; then                             # and if that fails
- echo "Mailserv is not yet supported on OpenBSD `uname -r`, please use a supported version of OpenBSD"
- exit 1
-fi
+if [ "$branch_changes" != "" ]; then
+  echo " "
+  echo "Code has been changed manually in Devel branch, system will not replace it from git hub." 
+  echo "Please add file(s) and commit changes"
+  echo "    cd /var/mailserv "
+  echo "    git add <filename>"
+  echo "    git commit -a" 
+  echo " " 
+  echo "This file(s) has been changed or added:"
+  echo " $branch_changes "
+  echo " "
+  exit 1 
+else
+  # switch to branch  
+  git --git-dir=/var/mailserv/.git --work-tree=/var/mailserv checkout $checkout_switch 2>/dev/null                 
 
- 
+  if [[ `echo $?` -ne 0 ]]; then                             # and if that fails
+   echo "Mailserv is not yet supported on OpenBSD `uname -r`, please use a supported version of OpenBSD or --devel parameter"
+   exit 1
+  fi
+fi 
+
+
 if [[ ! -d /usr/X11R6 ]]; then
   echo "You need to install the xbaseXX.tgz package for this to work"
   exit 1
