0c2313f5a296893797e2479a4edbad98190515ad
Marcus Bointon <marcus@synchromedia.co.uk>
2013-05-03 15:39:33 +0200

    Do initial clamav database download in background as it's ofen very slow

--- install/scripts/25_add_clamdb.sh
+++ install/scripts/25_add_clamdb.sh
@@ -6,6 +6,8 @@ if [ ! -f /var/db/clamav/main.cld ]; then
   touch /var/log/clam-update.log && chown _clamav:_clamav /var/log/clam-update.log
   touch /var/log/freshclam.log && chown _clamav:_clamav /var/log/freshclam.log
   
-  /usr/local/bin/freshclam --no-warnings
+  mkdir -p /var/db/clamav
   chown -R _clamav:_clamav /var/db/clamav
+  # Do initial download for clamav in background as it's often very slow
+  /usr/local/bin/freshclam --no-warnings &
 fi
