6e6a0518fce51613995673364e9f566c3ef7b1f6
David Gnojek <david@gnojek.net>
2013-07-01 16:59:32 +0200

    ramdisk mount command and fstab row updated

--- install/scripts/20_install_etc.sh
+++ install/scripts/20_install_etc.sh
@@ -12,9 +12,9 @@ install /var/mailserv/install/templates/fs/mailserv/* /usr/local/share/mailserv
 
 # Create a 64M RAM disk to keep PHP sessions in
 mkdir -p /tmp/phpsessions
-mount_mfs -o rw,async,nodev,noexec,nosuid -s 131072 /dev/wd0b /tmp/phpsessions
+mount_mfs -s 131072 -o rw,async,nodev,noexec,nosuid swap /tmp/phpsessions
 chown -R www:www /tmp/phpsessions
-echo "/dev/wd0b /tmp/phpsessions mfs rw,async,nodev,noexec,nosuid,-s=131072 0 0" >> /etc/fstab
+echo "swap /tmp/phpsessions mfs rw,async,nodev,noexec,nosuid,-s=131072 0 0" >> /etc/fstab
 
 template="/var/mailserv/install/templates"
 install -m 644 \
