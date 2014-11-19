5ae79fec332f8889ece5372133ed7d67a6b6d05d
dagn <dgnojek@gmail.com>
2014-11-19 17:15:11 +0100

    Removed ruby-rrd from scripts and replaced with standard system rrdtool because of problem with libpthread.so.18 and ruby since obsd55. Scripts was modified to call rrdtool through system.

--- scripts/rrdmon_create.rb
+++ scripts/rrdmon_create.rb
@@ -1,5 +1,9 @@
 #!/usr/local/bin/ruby
-require "RRD"
+#require "RRD"
+#ruby RRD has been disabled
+require 'rubygems'
+require 'date'
+
 
 rrd_dir = "/var/spool/rrd"
 start = Time.now.to_i - 1
@@ -15,18 +19,18 @@ start = Time.now.to_i - 1
 # Store 1 datapoint every 1 day and keep 1 year
 cpu_rrd = "#{rrd_dir}/cpu.rrd"
 unless File.exists?(cpu_rrd)
-  RRD.create(
-    cpu_rrd,
-    "--start", "#{start}",
-    "--step", "300",
-    "DS:user:GAUGE:400:0:100",
-    "DS:system:GAUGE:400:0:100",
-    "DS:idle:GAUGE:400:0:100",
-    "RRA:AVERAGE:0.5:1:288",
-    "RRA:AVERAGE:0.5:6:336",
-    "RRA:AVERAGE:0.5:24:372",
-    "RRA:AVERAGE:0.5:288:365"
-  )
+system <<-eos
+rrdtool create #{cpu_rrd} \
+--start #{start} \
+--step 300 \
+DS:user:GAUGE:400:0:100 \
+DS:system:GAUGE:400:0:100 \
+DS:idle:GAUGE:400:0:100 \
+RRA:AVERAGE:0.5:1:288 \
+RRA:AVERAGE:0.5:6:336 \
+RRA:AVERAGE:0.5:24:372 \
+RRA:AVERAGE:0.5:288:365
+eos
 end
 
 # Accept data every 5 minutes
@@ -36,18 +40,19 @@ end
 # Store 1 datapoint every 1 day and keep 1 year
 mem_rrd = "#{rrd_dir}/mem.rrd"
 unless File.exists?(mem_rrd)
-  RRD.create(
-    mem_rrd,
-    "--start", "#{start}",
-    "--step", "300",
-    "DS:usage:GAUGE:400:0:U",
-    "DS:free:GAUGE:400:0:U",
-    "RRA:AVERAGE:0.5:1:288",
-    "RRA:AVERAGE:0.5:6:336",
-    "RRA:AVERAGE:0.5:24:372",
-    "RRA:AVERAGE:0.5:288:365"
-  )
+system <<-eos
+rrdtool create #{mem_rrd} \
+--start #{start} \
+--step 300 \
+DS:usage:GAUGE:400:0:U \
+DS:free:GAUGE:400:0:U \
+RRA:AVERAGE:0.5:1:288 \
+RRA:AVERAGE:0.5:6:336 \
+RRA:AVERAGE:0.5:24:372 \
+RRA:AVERAGE:0.5:288:365
+eos
 end
+
 
 # Accept data every 5 minutes
 # Store 1 datapoint every 5 minute and keep 1 day
@@ -56,18 +61,19 @@ end
 # Store 1 datapoint every 1 day and keep 1 year
 swap_rrd = "#{rrd_dir}/swap.rrd"
 unless File.exists?(swap_rrd)
-  RRD.create(
-    swap_rrd,
-    "--start", "#{start}",
-    "--step", "300",
-    "DS:usage:GAUGE:400:0:U",
-    "DS:free:GAUGE:400:0:U",
-    "RRA:AVERAGE:0.5:1:288",
-    "RRA:AVERAGE:0.5:6:336",
-    "RRA:AVERAGE:0.5:24:372",
-    "RRA:AVERAGE:0.5:288:365"
-  )
+system <<-eos 
+rrdtool create #{swap_rrd} \
+--start #{start} \
+--step 300 \
+DS:usage:GAUGE:400:0:U \
+DS:free:GAUGE:400:0:U \
+RRA:AVERAGE:0.5:1:288 \
+RRA:AVERAGE:0.5:6:336 \
+RRA:AVERAGE:0.5:24:372 \
+RRA:AVERAGE:0.5:288:365
+eos
 end
+
 
 # Accept data every 5 minutes
 # Store 1 datapoint every 5 minute and keep 1 day
@@ -76,19 +82,19 @@ end
 # Store 1 datapoint every 1 day and keep 1 year
 mail_rrd = "#{rrd_dir}/mail.rrd"
 unless File.exists?(mail_rrd)
-  RRD.create(
-    mail_rrd,
-    "--start", "#{start}",
-    "--step", "300",
-    "DS:sent:GAUGE:400:0:U",
-    "DS:received:GAUGE:400:0:U",
-    "DS:bounced:GAUGE:400:0:U",
-    "DS:rejected:GAUGE:400:0:U",
-    "DS:virus:GAUGE:400:0:U",
-    "DS:spam:GAUGE:400:0:U",
-    "RRA:AVERAGE:0.5:1:288",
-    "RRA:AVERAGE:0.5:6:336",
-    "RRA:AVERAGE:0.5:24:372",
-    "RRA:AVERAGE:0.5:288:365"
-  )
+system <<-eos 
+rrdtool create #{mail_rrd} \
+--start #{start} \
+--step 300 \
+DS:sent:GAUGE:400:0:U \
+DS:received:GAUGE:400:0:U \
+DS:bounced:GAUGE:400:0:U \
+DS:rejected:GAUGE:400:0:U \
+DS:virus:GAUGE:400:0:U \
+DS:spam:GAUGE:400:0:U \
+RRA:AVERAGE:0.5:1:288 \
+RRA:AVERAGE:0.5:6:336 \
+RRA:AVERAGE:0.5:24:372 \
+RRA:AVERAGE:0.5:288:365
+eos
 end
