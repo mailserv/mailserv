94925c32a30f0d2e090d70f14942ea4f06187c60
mlaarhoven <github@martijnlaarhoven.nl>
2016-04-06 15:25:06 +0200

    PHP ini cleanup
    No apache module
    fix php module links
    use default php-5.6.ini and mailserv.ini

--- install/scripts/32_php.sh
+++ install/scripts/32_php.sh
@@ -3,32 +3,30 @@
 # Only run on install
 if [[ "$1" == "install" ]]; then
 
-  # Install php.ini file (this is a stock php.ini-production)
-  /usr/bin/install -m 644 /var/mailserv/install/templates/php-5.6.ini /etc/php-5.6/
-  
-  #To enable the php module please create a symbolic link
-  mkdir /var/www/conf/modules
-  ln -fs /var/www/conf/modules.sample/php-5.6.conf /var/www/conf/modules/php.conf
-
   # Install our local changes to php.ini
   /usr/bin/install -m 644 /var/mailserv/install/templates/php-mailserv.ini /etc/php-5.6/mailserv.ini
 
   # Install php-fpm.conf (replace fast-cgi)
   /usr/bin/install -m 644 /var/mailserv/install/templates/php-fpm.conf /etc/ 
  
-  # symlink for mysql 
-  ln -fs /etc/php-5.6.sample/mysql.ini /etc/php-5.6/mysql.ini
+  # Make php easier to run from CLI
+  ln -s /usr/local/bin/php-5.6 /usr/local/bin/php
+
+  # symlink for mysqli
+  ln -sf /etc/php-5.6.sample/mysqli.ini /etc/php-5.6/mysqli.ini
 
   # Symlink for mcrypt extension
   ln -sf /etc/php-5.6.sample/mcrypt.ini /etc/php-5.6/mcrypt.ini 
 
-  # PHP OPcache config 
-  /usr/bin/install -m 644 /var/mailserv/install/templates/opcache.ini /etc/php-5.6/
-
-  # Make php easier to run from CLI
-  ln -s /usr/local/bin/php-5.6 /usr/local/bin/php
+  # Symlink for opcache
+  ln -sf /etc/php-5.6.sample/opcache.ini /etc/php-5.6/opcache.ini
 
   #PHP Data Objects (PDO) for accessing databases in PHP (required by roundcube >0.9)
-  ln -fs /etc/php-5.6.sample/pdo_mysql.ini  /etc/php-5.6/pdo_mysql.ini
+  ln -sf /etc/php-5.6.sample/pdo_mysql.ini /etc/php-5.6/pdo_mysql.ini
 
+  # Symlink for gd
+  ln -sf /etc/php-5.6.sample/gd.ini /etc/php-5.6/gd.ini
+  
+  # Symlink for memcache
+  ln -sf /etc/php-5.6.sample/memcache.ini /etc/php-5.6/memcache.ini
 fi
