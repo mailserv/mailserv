40589428cb21af39c4f5594a68ffcee2f0bea639
mlaarhoven <github@martijnlaarhoven.nl>
2016-04-14 20:26:04 +0200

    mysqld test

--- admin/lib/mailserver.rb
+++ admin/lib/mailserver.rb
@@ -5,7 +5,7 @@ class Mailserver
       :clamd      => %x{ps -ax | egrep clamd | grep -v grep | wc -l}.to_i > 0,
       :postfix    => system('rcctl check postfix'),
       :dovecot    => system('rcctl check dovecot'),
-      :mysqld     => system('rcctl start mysqld'),
+      :mysqld     => system('rcctl check mysqld'),
       :spamd      => %x{ps -ax | egrep spamd | grep -v grep | wc -l}.to_i > 0,
       :freshclam  => %x{ps -ax | egrep freshclam | grep -v grep | wc -l}.to_i > 0,
       :dnsmasq    => system('rcctl check dnsmasq'),
