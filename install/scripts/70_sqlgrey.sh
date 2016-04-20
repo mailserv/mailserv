7de0e22af84c4ed85ef1caa97b5378d2fd56181a
mlaarhoven <github@martijnlaarhoven.nl>
2016-04-20 17:42:23 +0200

    sqlgrey
    Use rcctl

--- install/scripts/70_sqlgrey.sh
+++ install/scripts/70_sqlgrey.sh
@@ -5,7 +5,8 @@ if [[ "$1" == "install" ]]; then
   install -m 644 /var/mailserv/install/templates/sqlgrey.conf /etc/sqlgrey/
   /usr/local/bin/mysqladmin create sqlgrey
   /usr/local/bin/mysql -e "grant all privileges on sqlgrey.* to 'sqlgrey'@'localhost' identified by 'sqlgrey';"
-  /usr/local/sbin/sqlgrey -d
+  rcctl enable sqlgrey
+  rcctl start  sqlgrey
   sleep 2
   /usr/local/bin/mysql sqlgrey -e "alter table connect add id int primary key auto_increment first;"
   touch /etc/sqlgrey/clients_fqdn_whitelist.local 
