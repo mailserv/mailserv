31931c5984844d25407e1b8547342b433a815417
mlaarhoven <mlaarhoven@users.noreply.github.com>
2020-12-09 16:35:06 +0100

    Version 6.8

--- install/scripts/10_pkg_add.sh
+++ install/scripts/10_pkg_add.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ X"$PKG_PATH" == X"" ]; then
-  export PKG_PATH=http://ftp.OpenBSD.org/pub/OpenBSD/`uname -r`/packages/`uname -m`/
-  grep PKG_PATH /etc/profile || echo "export PKG_PATH=http://ftp.OpenBSD.org/pub/OpenBSD/`uname -r`/packages/`uname -m`/" >> /etc/profile
+if [ ! -f /etc/installurl ]; then
+  echo "Install URL"
+  echo 'https://ftp.openbsd.org/pub/OpenBSD/' > /etc/installurl
 fi
 
 case $1 in
@@ -17,31 +17,29 @@ Fetching versions:
 
 __EOT
     pkg_add -v -m -I \
-     postfix-3.0.3p0-mysql \
+     postfix-3.6.20200627-mysql \
      clamav \
-     gnupg-2.1.9 \
+     gnupg-2.2.23p0 \
      p5-Mail-SPF \
      p5-Mail-SpamAssassin \
      rrdtool \
-     ruby-gems \
-     ruby-rake \
-     ruby-iconv \
+     ruby-2.7.1p1
      dnsmasq \
      dovecot-pigeonhole \
      dovecot-mysql \
      memcached-- \
      mariadb-server \
-     nginx-- \
+     nginx \
      openssl \
      sqlgrey \
      gsed \
      gtar--static \
-     php-5.6.18 \
-	 php-intl-5.6.18 \
-     php-mysqli-5.6.18 \
-     php-pdo_mysql-5.6.18 \
-     php-gd-5.6.18 \
-     php-mcrypt-5.6.18 \
+     php-7.4.13 \
+	   php-intl-7.4.13 \
+     php-mysqli-7.4.13 \
+     php-pdo_mysql-7.4.13 \
+     php-gd-7.4.13 \
+     php-mcrypt-7.4.13 \
      ghostscript-fonts \
      ghostscript--no_x11 \
      ImageMagick \
