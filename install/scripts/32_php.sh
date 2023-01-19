76ccf1bf18c5ee3bf90fce79f5a48abed1c9cf2c
mlaarhoven <mlaarhoven@users.noreply.github.com>
2023-01-19 15:54:01 +0100

    PHP 8.1

--- install/scripts/32_php.sh
+++ install/scripts/32_php.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
 # Only run on install
-if [[ "$1" == "install" ]]; then
+[[ "$1" != "install" ]] && exit 1
 
-  # Create symlinks for all installed php extensions
-  cd /etc/php-5.6.sample
-  for i in *; do ln -sf ../php-5.6.sample/$i ../php-5.6/; done
+# Create symlinks for all installed php extensions
+cd /etc/php-8.1.sample
+for i in *; do ln -sf ../php-8.1.sample/$i ../php-8.1/; done
 
-  # Install our local changes to php.ini
-  /usr/bin/install -m 644 /var/mailserv/install/templates/php-mailserv.ini /etc/php-5.6/mailserv.ini
+# Install our local changes to php.ini
+/usr/bin/install -m 644 /var/mailserv/install/templates/php-mailserv.ini /etc/php-8.1/mailserv.ini
 
-  # Install php-fpm.conf
-  /usr/bin/install -m 644 /var/mailserv/install/templates/php-fpm.conf /etc/ 
+# Install php-fpm.conf
+/usr/bin/install -m 644 /var/mailserv/install/templates/php-fpm.conf /etc/ 
 
-  # Make php easier to run from CLI
-  ln -s /usr/local/bin/php-5.6 /usr/local/bin/php
+# Make php easier to run from CLI
+ln -s /usr/local/bin/php-8.1 /usr/local/bin/php
 
-  rcctl enable php56_fpm
-  rcctl start  php56_fpm
-fi
+rcctl enable php81_fpm
+rcctl start  php81_fpm
